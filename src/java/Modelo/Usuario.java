ce912a0f768feeaa17bc548aca88ee3157bab0ec
Gabriel <Gabriel@10.82.118.188>
2013-11-19 17:29:21 +0100

    Clase usuario con los commit y los contructores

--- src/java/Modelo/Usuario.java
+++ src/java/Modelo/Usuario.java
@@ -3,7 +3,6 @@
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
-
 package Modelo;
 
 /**
@@ -11,6 +10,7 @@ package Modelo;
  * @author Gabriel
  */
 public class Usuario {
+
     int id;
     String nombre;
     String apellidos;
@@ -19,5 +19,50 @@ public class Usuario {
     String password;
     String nick;
     String avatar;
+
+    public Usuario(){}
+
+    public Usuario(int id, String nombre, String apellidos, String email, int rol, String password, String nick, String avatar) {
+        this.id = id;
+        this.nombre = nombre;
+        this.apellidos = apellidos;
+        this.email = email;
+        this.rol = rol;
+        this.password = password;
+        this.nick = nick;
+        this.avatar = avatar;
+    }    
     
+    public int getId() {
+        return id;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public String getApellidos() {
+        return apellidos;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public int getRol() {
+        return rol;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public String getNick() {
+        return nick;
+    }
+
+    public String getAvatar() {
+        return avatar;
+    }
+
 }
